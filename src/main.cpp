90b72044b3cd3acd9d586cbf95be6134e040fff4
Helal Anwar <hilal123anwar@gmail.com>
2023-06-02 23:34:54 +0530

    Added some new function

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include "World.h"
+int main(int arg,char const *args[]) {
+    int a, b, c;
+    printf_s("Enter the 3 numbers \n");
+    scanf_s("%d %d %d", &a, &b, &c);
+    printf_s("The three number that are entered are \n");
+    printf_s("%d\n", a);
+    printf_s("%d\n", b);
+    printf_s("%d\n", c);
+    printf_s("Hello ! world \n");
+    for (int i = 1; i < arg; ++i) {
+        printf_s("%s \n",args[i]);
+    }
+    _generatePython_triplet(3, 20000000);
+    return 0;
+}
