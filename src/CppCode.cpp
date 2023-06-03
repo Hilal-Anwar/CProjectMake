e78b2a0c43c8c368da11d6485618953173fabeef
Helal Anwar <hilal123anwar@gmail.com>
2023-06-03 20:52:06 +0530

    Added some new function

--- src/CppCode.cpp
+++ src/CppCode.cpp
@@ -6,10 +6,6 @@
 
 using namespace std;
 
-void sp_to_dash(const char *str);
-void array_entry(int *a);
-void find_sum();
-void math_library_use();
 
 void find_sum()
 {
@@ -44,5 +40,51 @@ void math_library_use()
   cout << sin(30) << "\n";
   cout << cos(60) << "\n";
   cout << sqrt(5) << "\n";
+}
 
+int getHcf(int a, int b)
+{
+  int mx = max(a, b);
+  int mi = min(a, b);
+  while (mx % mi)
+  {
+    int temp = mi;
+    mi = mx % mi;
+    mx = temp;
+  }
+  return mi;
+}
+int c_factor(int *a)
+{
+  return getHcf(getHcf(a[0], a[1]), a[2]);
+}
+void _generatePython_triplet(int start, int end)
+{
+  int m = (start - 1) / 2;
+  int n = m + 1;
+  int a, b, c;
+  int cr = 0;
+  while ((n * n + m * m) <= end)
+  {
+    int i = n;
+    for (; (m * m + i * i) <= end; i++)
+    {
+      a = i * i - m * m;
+      b = 2 * m * i;
+      c = m * m + i * i;
+      int k = 1;
+      int varg[] = {a,b,c};
+      if (c_factor(varg) == 1)
+      {
+        while (c * k <= end)
+        {
+          k += 1;
+          cr++;
+        }
+      }
+    }
+    m = m + 1;
+    n = n + 1;
+  }
+  cout << "Number of Pythagorean triplet " << cr << "\n";
 }
--- src/World.h
+++ src/World.h
@@ -1,1 +1,8 @@
 int sum_finder();
+void sp_to_dash(const char *str);
+void array_entry(int *a);
+void find_sum();
+void math_library_use();
+int getHcf(int a, int b);
+int c_factor(int *a);
+void _generatePython_triplet(int start, int end);
